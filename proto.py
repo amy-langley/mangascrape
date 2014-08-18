827bffadb7e734f54af98586ccad537540ecd316
Amy Rebecca <none@none>
2014-08-17 23:09:14 -0500

    more tinkering

--- proto.py
+++ proto.py
@@ -1,5 +1,19 @@
 from lxml import html
 import requests
+from urlparse import urlparse
+from urlparse import urlunparse
+from utils.mkdtemp import mkdtemp
+
+def download_file(url):
+    local_filename = urlparse(url).path.split('/')[-1]
+    # NOTE the stream=True parameter
+    r = requests.get(url, stream=True)
+    with open(local_filename, 'wb') as f:
+        for chunk in r.iter_content(chunk_size=1024):
+            if chunk:  # filter out keep-alive new chunks
+                f.write(chunk)
+                f.flush()
+    return local_filename
 
 #page = requests.get('http://www.mangahere.co/mangalist/')
 #tree = html.fromstring(page.text)
@@ -8,13 +22,20 @@ import requests
 #items = [[i.text_content(), i.get('href')] for i in titlelinks]
 #items = [[i.text_content(), i.get('href')] for i in titlelinks if 'Hourou' in i.text_content()]
 
-#print items
+# print items
 
 
-page2 = requests.get('http://www.mangahere.co/manga/hourou_musuko/')
-tree2 = html.fromstring(page2.text)
-chapterlinks = tree2.cssselect('div.detail_list ul span.left a')
+#page2 = requests.get('http://www.mangahere.co/manga/hourou_musuko/')
+#tree2 = html.fromstring(page2.text)
+#chapterlinks = tree2.cssselect('div.detail_list ul span.left a')
 
-items = [i.get('href') for i in chapterlinks]
+#items = [i.get('href') for i in chapterlinks]
+
+page3 = requests.get('http://www.mangahere.co/manga/hourou_musuko/v01/c001/')
+tree3 = html.fromstring(page3.text)
+items = [i.get('src') for i in tree3.cssselect('section.read_img img')]
+
+for item in items:
+    download_file(item)
 
 print items
---
+++ utils/mkdtemp.py
@@ -0,0 +1,17 @@
+class mkdtemp(object):
+
+    def __init__(self, *args, **kwargs):
+        self.should_chdir = kwargs.pop("chdir")
+        self.original_dir = None
+        self.dirname = tempfile.mkdtemp(*args, **kwargs)
+
+    def __enter__(self):
+        if self.should_chdir:
+            self.original_dir = os.getcwd()
+            os.chdir(self.dirname)
+        return self.dirname
+
+    def __exit__(self, *exc):
+        if self.original_dir:
+            os.chdir(self.original_dir)
+        shutil.rmtree(self.dirname)
