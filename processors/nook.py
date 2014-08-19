f8e91c18d0f82c609d1b59dfa5914d7538ff3bc5
Amy Rebecca <none@none>
2014-08-18 21:17:13 -0500

    added image manipulation library

---
+++ processors/nook.py
@@ -0,0 +1,10 @@
+class Dimensions:
+    # 70px of vertical space for UI
+    DEFAULT = (600, 730)
+    SIMPLE_TOUCH = (600, 730)
+    GLOW_LIGHT = (758, 954)
+    HD = (900, 1370)
+    HD_PLUS = (1280, 1850)
+    FIRST_EDITION = (600, 730)
+    COLOR = (600, 954)
+    TABLET = (600, 954)
